521a1c3ed5a7d7190278502e03266a76e3e8aa0e
Vivek Jain <vivekjain10@gmail.com>
2013-07-12 17:39:43 -0400

    Install and enable opencart in apache

--- manifests/web.pp
+++ manifests/web.pp
@@ -21,3 +21,33 @@ package { "php5":
     require =>  Package["apache2"],
     notify  =>  Service["apache2"],
 }
+
+package { "wget":
+  ensure  => "installed",
+}
+
+exec {"has-opencart.deb":
+    command =>  "/usr/bin/wget http://192.168.1.145/opencart.deb -O /tmp/opencart.deb",
+    creates =>  "/tmp/opencart.deb",
+    require =>  Package["wget"],
+}
+
+package{"opencart":
+    ensure    =>  latest,
+    provider  =>  dpkg,
+    source    =>  "/tmp/opencart.deb",
+    require   =>  [Exec["has-opencart.deb"], Package["php5-mysql", "php5-gd", "php5-curl"]],
+}
+
+file {"/etc/apache2/sites-enabled/000-default":
+    ensure  =>  absent,
+    require =>  Package["apache2"],
+    notify  =>  Service["apache2"],
+}
+
+file{"/etc/apache2/sites-enabled/opencart":
+    ensure  =>  link,
+    target  =>  "/etc/apache2/sites-available/opencart",
+    require =>  Package["opencart"],
+    notify  =>  Service["apache2"],
+}
