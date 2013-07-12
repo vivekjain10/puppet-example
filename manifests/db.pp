38151c6d02564b1f8ac41dd5c4330f41c7c373e9
Vivek Jain <vivekjain10@gmail.com>
2013-07-12 13:51:39 -0400

    Add mysql's allow_external

--- manifests/db.pp
+++ manifests/db.pp
@@ -16,3 +16,10 @@ Exec["apt-get update"] -> Package <||>
 package { "mysql-server":
     ensure => installed,
 }
+
+file {"/etc/mysql/conf.d/allow_external.cnf":
+    owner =>  mysql,
+    group =>  mysql,
+    mode  =>  0644,
+    content =>  "[mysqld]\n    bind-address = 0.0.0.0",
+}
